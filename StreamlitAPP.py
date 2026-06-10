d3f329eab1c26eb4d99963bf4425400e697bead0
PRAKHAR SAXENA <prakharsaxena230706@gmail.com>
2026-06-10 22:45:27 +0530

    Update StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -5,7 +5,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.MCQ_Generator.utils import read_file, get_table_data
 import streamlit as st
-
+import json
 from langchain_community.callbacks import get_openai_callback
 from src.MCQ_Generator.MCQ_Generate import generate_evaluate_chain
 from src.MCQ_Generator.logger import logging
